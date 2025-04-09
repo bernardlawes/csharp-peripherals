e5a5fbc2d23be6300d7e02e73074e69719616033
Bernard Lawes <bernardlawes@gmail.com>
2025-04-09 02:30:54 -0400

    Auto-commit from push script

---
+++ ClipboardMonitor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_peripherals
+{
+    public class ClipboardMonitor : Form
+    {
+        public event Action<string> OnClipboardTextChanged;
+
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        [DllImport("user32.dll")]
+        private static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            AddClipboardFormatListener(this.Handle);
+        }
+
+        protected override void OnHandleDestroyed(EventArgs e)
+        {
+            RemoveClipboardFormatListener(this.Handle);
+            base.OnHandleDestroyed(e);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                if (Clipboard.ContainsText())
+                {
+                    string text = Clipboard.GetText();
+                    OnClipboardTextChanged?.Invoke(text);
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
---
+++ ClipboardMonitorHost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_peripherals
+{
+    public static class ClipboardMonitorHost
+    {
+        private static Thread _thread;
+        private static ClipboardMonitor _monitor;
+
+        public static void Start(Action<string> onClipboardChanged)
+        {
+            if (_thread != null && _thread.IsAlive)
+                return;
+
+            _thread = new Thread(() =>
+            {
+                _monitor = new ClipboardMonitor();
+                _monitor.OnClipboardTextChanged += onClipboardChanged;
+                Application.Run(_monitor);
+            });
+
+            _thread.SetApartmentState(ApartmentState.STA);
+            _thread.IsBackground = true;
+            _thread.Start();
+        }
+
+        public static void Stop()
+        {
+            if (_monitor != null && !_monitor.IsDisposed)
+            {
+                _monitor.Invoke(new Action(() => _monitor.Close()));
+            }
+        }
+    }
+}
